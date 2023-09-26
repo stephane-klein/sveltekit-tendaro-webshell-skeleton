8e13a8351669092c64c304071b8ec8ed28ed143a
Stéphane Klein <contact@stephane-klein.info>
2023-09-26 13:47:56 +0200

    Add SectionTitle first iteration

---
+++ src/lib/SectionTitle.story.svelte
@@ -0,0 +1,13 @@
+<script>
+    import SectionTitle from "./SectionTitle.svelte";
+
+    export let Hst;
+</script>
+
+<Hst.Story>
+    <SectionTitle as="h2">Project Activity</SectionTitle>
+
+    <SectionTitle align="left">Project Activity</SectionTitle>
+
+    <SectionTitle align="center"><span class="text-xs">Project Activity</span></SectionTitle>
+</Hst.Story>
---
+++ src/lib/SectionTitle.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let as = "h3";
+    export let align = "left";
+</script>
+
+<svelte:element
+    this={as}
+    class={`relative inline-block w-full ${align == "center" ? "text-center" : ""} ${$$props.class}`}
+>
+    <div class="absolute w-full top-1/2 border-b border-b-gray-600 -z-10"></div>
+    <span class={`bg-white ${align == "left" ? "pr-2" : "px-2"}`}><slot /></span>
+</svelte:element>
